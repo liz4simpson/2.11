5ae729a1324c13c00b8ae052b9c371afc028aa6f
liz4simpson <k4shenko.2019@gmail.com>
2022-12-26 14:25:59 +0300

    added

---
+++ progs/ind.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+
+def surname(s):
+    def name(n):
+        a = 'Уважаемая, {} {}! ' \
+            'Вы делаете работу по замыканиям функций.'.format(s, n)
+        return a
+    return name
+
+
+if __name__ == '__main__':
+    p = surname('Яковлева')
+    print(p('Елизавета'))
